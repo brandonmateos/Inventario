45081e2daed9b13a1c3fe1b369c86f6abe8fc651
Brandon Mateos <blozano0@ucol.mx>
2022-10-04 20:26:36 -0500

    V1

--- src/producto.js
+++
@@ -1,8 +0,0 @@
-export class Producto {
-    constructor(codigo, nombre, precio, cantidad) {
-        this.codigo = codigo;
-        this.nombre = nombre;
-        this.precio = precio;
-        this.cantidad = cantidad;
-    }
-}
